4793ba4b0a489bfa9e20ac9151df1474286be256
Danil Fomin <fominitstep@gmail.com>
2018-06-23 23:59:36 +0300

    almost done commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,11 +14,8 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot(UrlGenerator $url)
-    {
-        if(env('APP_ENV') !== 'local')
-        {
-            $url->forceScheme('https');
-        }
+    {        
+        $url->forceScheme('https');
 
         Schema::defaultStringLength(191);
     }
